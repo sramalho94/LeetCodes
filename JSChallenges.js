8eed40a6cb900286e53bcdb3e2c1ba0220f6075e
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-23 10:42:29 -0500

    added answer to #557

---
+++ JSChallenges.js
@@ -0,0 +1,15 @@
+// 557. Reverse Words in a String III
+var reverseWords = function (s) {
+  let answer = []
+  let revWord = ''
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ' ') {
+      answer.push(revWord.split('').reverse().join(''))
+      revWord = ''
+    } else {
+      revWord += s[i]
+    }
+  }
+  answer.push(revWord.split('').reverse().join(''))
+  return answer.join(' ')
+}
