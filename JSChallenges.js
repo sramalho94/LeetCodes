ffcf375ddd3f2ebe2a6eef783002b0b7934536bd
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-28 12:27:56 -0500

    141. Linked List Cycle

--- JSChallenges.js
+++ JSChallenges.js
@@ -50,6 +50,20 @@ var maximumValue = function (strs) {
   return val
 }
 
+// 141. Linked List Cycle
+var hasCycle = function (head) {
+  slow = head
+  fast = head
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+    if (slow === fast) {
+      return true
+    }
+  }
+  return false
+}
+
 // 142. Linked List Cycle II
 
 var detectCycle = function (head) {
