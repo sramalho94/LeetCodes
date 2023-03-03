ae2a39cedadc5a7dd86b463ffd9e6038f3a492fc
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-03 10:40:38 -0500

    solution for number 28

--- PythonChallenges.py
+++ PythonChallenges.py
@@ -8,3 +8,15 @@ def convertTemperature(self, celsius: float) -> List[float]:
         ans.append(Fahr)
 
         return ans
+
+# 70. Climbing Stairs
+def mySqrt(self, x: int) -> int:
+        i = 0
+        num =0
+        while num <=x:
+            num = i * i
+            if num > x:
+                return i -1
+            elif num == x:
+                return i
+            i += 1
