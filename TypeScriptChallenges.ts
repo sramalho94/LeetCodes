405d081a6ab80dc421a22dd07927216539c349a0
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-28 15:11:17 -0400

    solution for 238 in TS

--- TypeScriptChallenges.ts
+++ TypeScriptChallenges.ts
@@ -241,3 +241,26 @@ function isSubsequence(s: string, t: string): boolean {
   }
   return pointer === s.length
 }
+// 238. Product of Array Except Self
+function productExceptSelf(nums: number[]): number[] {
+  let left: number[] = new Array(nums.length)
+  let right: number[] = new Array(nums.length)
+  let answer: number[] = new Array(nums.length)
+
+  left[0] = 1
+  right[nums.length - 1] = 1
+
+  for (let i: number = 1; i < nums.length; i++) {
+    left[i] = nums[i - 1] * left[i - 1]
+  }
+
+  for (let i = nums.length - 2; i >= 0; i--) {
+    right[i] = nums[i + 1] * right[i + 1]
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = left[i] * right[i]
+  }
+
+  return answer
+}
