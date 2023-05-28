401f9d492b4e2094581600ec6443e88acd0ad2af
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-28 15:13:36 -0400

    solution for 643 in TS

--- TypeScriptChallenges.ts
+++ TypeScriptChallenges.ts
@@ -282,3 +282,20 @@ function increasingTriplet(nums: number[]): boolean {
 
   return false
 }
+// 643. Maximum Average Subarray I
+function findMaxAverage(nums: number[], k: number): number {
+  let maxSum: number = 0
+  let currentSum: number = 0
+
+  for (let i = 0; i < k; i++) {
+    maxSum += nums[i]
+  }
+  currentSum = maxSum
+
+  for (let i = k; i < nums.length; i++) {
+    currentSum = currentSum - nums[i - k] + nums[i]
+    maxSum = Math.max(maxSum, currentSum)
+  }
+
+  return maxSum / k
+}
