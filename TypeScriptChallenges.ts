862cec00fad7a3dbc0f76914200442e64ac8ad69
Stephan D Ramalho <sramalho@fordham.edu>
2023-06-20 16:00:47 -0400

    412 in TS

--- TypeScriptChallenges.ts
+++ TypeScriptChallenges.ts
@@ -414,3 +414,20 @@ function findTheDifference(s: string, t: string): string {
     }
   }
 }
+
+// 412. Fizz Buzz
+function fizzBuzz(n: number): string[] {
+  let answer: string[] = []
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      answer.push('FizzBuzz')
+    } else if (i % 3 === 0) {
+      answer.push('Fizz')
+    } else if (i % 5 === 0) {
+      answer.push('Buzz')
+    } else {
+      answer.push(i.toString())
+    }
+  }
+  return answer
+}
