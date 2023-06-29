725ec7b0761ae8e8178b530e670afdf8abeadb66
Stephan D Ramalho <sramalho@fordham.edu>
2023-06-29 11:20:30 -0400

    2697 in TS

--- TypeScriptChallenges.ts
+++ TypeScriptChallenges.ts
@@ -491,3 +491,50 @@ function findTheDifference(s: string, t: string): string {
     }
   }
 }
+// 2697. Lexicographically Smallest Palindrome
+function makeSmallestPalindrome(s: string): string {
+  let start = 0
+  let end = s.length - 1
+  let sArr: string[] = s.split('')
+  let charRecord: { [key: string]: number } = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26
+  }
+  while (start < end) {
+    if (sArr[start] !== sArr[end]) {
+      if (charRecord[sArr[start]] < charRecord[sArr[end]]) {
+        sArr[end] = sArr[start]
+      } else {
+        sArr[start] = sArr[end]
+      }
+    }
+    start++
+    end--
+  }
+
+  return sArr.join('')
+}
