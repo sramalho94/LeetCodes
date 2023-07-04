b28721d0c4d6b51d9955c60218910292cf31f8ac
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-04 12:24:39 -0400

    938 in TS

--- TypeScriptChallenges.ts
+++ TypeScriptChallenges.ts
@@ -593,3 +593,21 @@ function maxProduct(words: string[]): number {
   }
   return max
 }
+
+// 938. Range Sum of BST
+function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
+  let sum = 0
+  if (root === null) {
+    return 0
+  }
+  if (root.val >= low && root.val <= high) {
+    sum += root.val
+  }
+  if (root.val > low) {
+    sum += rangeSumBST(root.left, low, high)
+  }
+  if (root.val < high) {
+    sum += rangeSumBST(root.right, low, high)
+  }
+  return sum
+}
